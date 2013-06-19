6de6f052f4f22be2e6a72393ad9d197b299bc77b
samirdouichi <samirdouichi@samirdou-452d83>
2013-06-19 17:01:33 -0300

    fix some bugs

--- reference-app-services-model/src/main/java/com/sjo/mapper/ressource/MoyenDeTransportMapper.java
+++ reference-app-services-model/src/main/java/com/sjo/mapper/ressource/MoyenDeTransportMapper.java
@@ -14,31 +14,26 @@ import com.sjo.model.ressource.MoyenDeTransport;
  * @version 1.0
  * @created 17-Jun-2013 8:12:03 PM
  */
-public class MoyenDeTransportMapper implements RowMapper<MoyenDeTransportMapper>{
+public class MoyenDeTransportMapper implements RowMapper<MoyenDeTransport>{
+
+	
 
 	@Override
-	public MoyenDeTransportMapper mapRow(ResultSet arg0, int arg1)
+	public MoyenDeTransport mapRow(ResultSet resultSet, int rowNum)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		MoyenDeTransport moyenDeTransport=new MoyenDeTransport();
+		moyenDeTransport.setMoyenDeTransportID(Long.toString(resultSet.getLong("moyenDeTransportID")));
+		
+		moyenDeTransport.setChargeMax(resultSet.getInt("chargeMax"));
+		
+		moyenDeTransport.setChargeMin(resultSet.getInt("chargeMin"));
+//		equipement.setNumSerie(resultSet.getString("numSerie"));
+//
+//		equipement.setOrigineFabriquant(resultSet
+//				.getString("origineFabriquant"));
+//		equipement.setRefFabriquant(resultSet.getString("refFabriquant"));
+		return moyenDeTransport;
 	}
-
-//	@Override
-//	public MoyenDeTransport mapRow(ResultSet resultSet, int rowNum)
-//			throws SQLException {
-//		
-//		MoyenDeTransport moyenDeTransport=new MoyenDeTransport();
-//		moyenDeTransport.setMoyenDeTransportID(Long.toString(resultSet.getLong("moyenDeTransportID")));
-//		
-//		moyenDeTransport.setChargeMax(resultSet.getInt("chargeMax"));
-//		
-//		moyenDeTransport.setChargeMin(resultSet.getInt("chargeMin"));
-////		equipement.setNumSerie(resultSet.getString("numSerie"));
-////
-////		equipement.setOrigineFabriquant(resultSet
-////				.getString("origineFabriquant"));
-////		equipement.setRefFabriquant(resultSet.getString("refFabriquant"));
-//		return moyenDeTransport;
-//	}
 	
 }
